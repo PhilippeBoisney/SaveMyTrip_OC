163b5d3ddb4a3acdde6c0057b64460d32cbc8930
Boisney Philippe <phil.boisney@gmail.com>
2018-03-20 16:29:05 +0100

    Travis update

---
+++ app/src/main/java/com/openclassrooms/savemytrip/injections/ViewModelFactory.java
@@ -0,0 +1,35 @@
+package com.openclassrooms.savemytrip.injections;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+import com.openclassrooms.savemytrip.repositories.ItemDataRepository;
+import com.openclassrooms.savemytrip.repositories.UserDataRepository;
+import com.openclassrooms.savemytrip.todolist.ItemViewModel;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Created by Philippe on 20/03/2018.
+ */
+
+public class ViewModelFactory implements ViewModelProvider.Factory {
+
+    private final ItemDataRepository itemDataSource;
+    private final UserDataRepository userDataSource;
+    private final Executor executor;
+
+    public ViewModelFactory(ItemDataRepository itemDataSource, UserDataRepository userDataSource, Executor executor) {
+        this.itemDataSource = itemDataSource;
+        this.userDataSource = userDataSource;
+        this.executor = executor;
+    }
+
+    @Override
+    public <T extends ViewModel> T create(Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(ItemViewModel.class)) {
+            return (T) new ItemViewModel(itemDataSource, userDataSource, executor);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel class");
+    }
+}
